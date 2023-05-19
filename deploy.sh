e8d8be1fae5d5aae605292262342bac3516071d4
mehul <talpadaarjun4445@gmail.com>
2023-05-19 06:42:26 +0000

    commited

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@
 
 # Install requirements
 echo "[BUILD STARTED]"
-pip install -r requirements.txt
+pip install -r requirement.txt
 echo "[REQUIREMENTS INSTALLED]"
 # Collect static files
 python manage.py collectstatic --noinput
